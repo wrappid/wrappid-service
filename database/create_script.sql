ab1ed08ab6aef7a8945b8ee23c2e7a7caba79f8d
Pritam Mondal <pritammondal8981@gmail.com>
2023-08-16 15:37:53 +0530

    + CornJobsDBScripts
    #5

--- database/create_script.sql
+++ database/create_script.sql
@@ -15,3 +15,55 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."FileHandlers"
     OWNER to postgres;
+
+
+
+-- Table: public.CronSchemas
+
+-- DROP TABLE IF EXISTS public."CronSchemas";
+
+CREATE TABLE IF NOT EXISTS public."CronSchemas"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default",
+    expression text COLLATE pg_catalog."default",
+    "cronModule" text COLLATE pg_catalog."default",
+    "createdAt" time with time zone,
+    "updatedAt" time with time zone,
+    CONSTRAINT "Cronschemas_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."CronSchemas"
+    OWNER to postgres;
+
+
+
+-- Table: public.ApirequestLogs
+
+-- DROP TABLE IF EXISTS public."ApirequestLogs";
+
+CREATE TABLE IF NOT EXISTS public."ApirequestLogs"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    access_key text COLLATE pg_catalog."default",
+    endpoint text COLLATE pg_catalog."default",
+    request text COLLATE pg_catalog."default",
+    header_stack jsonb,
+    response jsonb,
+    start_ts timestamp with time zone,
+    created_ts timestamp with time zone,
+    updated_ts timestamp with time zone,
+    end_ts timestamp with time zone,
+    req_body jsonb,
+    ip text COLLATE pg_catalog."default",
+    response_status text COLLATE pg_catalog."default",
+    response_header jsonb,
+    CONSTRAINT api_request_logs_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."ApirequestLogs"
+    OWNER to postgres;
--- database/insert_script..sql
+++ database/insert_script..sql
@@ -1,3 +1,8 @@
 INSERT INTO public."FileHandlers"(
 	id, "fileUrl", "createdAt", "updatedAt")
 	VALUES (?, ?, ?, ?);
+
+
+INSERT INTO public."CronSchemas"(
+	id, name, expression, "cronModule", "createdAt", "updatedAt")
+	VALUES (?, ?, ?, ?, ?, ?);
