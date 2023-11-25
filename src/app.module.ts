f2bf62b17ccf2766eb325962ffbbeaf57dc84eb7
Pritam Mondal <pritammondal8981@gmail.com>
2023-11-25 16:06:42 +0530

    RootModule added to main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-// import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
+import { RootModule } from "@wrappid/service-core";
 // import { AppModule } from "@wrappid/service-core";
 
-// @Module({
-//   imports: [],
-//   controllers: [],
-//   providers: [],
-//   exports: [],
-// })
-// export class AppModule extends AppModule {}
+@Module({
+  imports: [RootModule],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from "@nestjs/core";
-// import { AppModule } from "./app.module";
-import { RootModule } from "@wrappid/service-core";
+import { AppModule } from "./app.module";
+// import { RootModule } from "@wrappid/service-core";
 /**
  * DO NOT TOUCH THIS FILE
  */
 async function bootstrap() {
-  const App = await NestFactory.create(RootModule);
+  const App = await NestFactory.create(AppModule);
   await App.listen(3000);
 }
 
