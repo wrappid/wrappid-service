fe149e7676695967da68c76e878c487b0a200796
jyoti1610 <jyotirmoyghosh07@gmail.com>
2024-03-04 18:21:45 +0530

    test(global): :white_check_mark: wrappid service API testing
    wrappid service API testing
    
    ref: #69

--- test/businessTablesDatabase.test.ts
+++ test/businessTablesDatabase.test.ts
@@ -60,4 +60,94 @@ describe("Test business/tables/:database", () => {
     expect(responseTime).toBeLessThanOrEqual(acceptableResponseTime);  //responsetime = 306ms as of now 
   });
 });
+describe("Test business/tables/:database", () => {
+  test("TC04 Verify API Response Headers", async () => {
+    const database = "application";
+    const response = await request(BASE_URL)
+      .get(`business/tables/${database}`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Content-Type", "application/json")
+      .set("Accept-Encoding", "gzip, deflate, br")
+      .set("Connection", "keep-alive")
+      .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+
+    expect(response.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(response.headers["access-control-allow-origin"]).toBe("*");
+    expect(response.headers["connection"]).toBe("keep-alive");
+  });
+});
+describe("Test business/tables/:database", () => {
+  test("TC04 Verify API Response Headers", async () => {
+    const database = "application";
+    const response = await request(BASE_URL)
+      .get(`business/tables/${database}`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Content-Type", "application/json")
+      .set("Accept-Encoding", "gzip, deflate, br")
+      .set("Connection", "keep-alive")
+      .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+
+    expect(response.headers["content-type"]).toBe("application/json; charset=utf-8");
+    expect(response.headers["access-control-allow-origin"]).toBe("*");
+    expect(response.headers["connection"]).toBe("keep-alive");
+  });
+});
+describe("Test business/tables/:database", () => {
+  test("TC05 Verify API Response Payload Size", async () => {
+    const database = "application";
+    const response = await request(BASE_URL)
+      .get(`business/tables/${database}`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Content-Type", "application/json")
+      .set("Accept-Encoding", "gzip, deflate, br")
+      .set("Connection", "keep-alive")
+      .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+
+    const payloadSize = Buffer.from(JSON.stringify(response.body)).length;
+    const maxSize = 2000; // Set the maximum allowed payload size in bytes
+    expect(payloadSize).toBeLessThanOrEqual(maxSize);
+  });
+});
+/*describe("Test business/tables/:database", () => {
+  test("TC05 Verify API Handles Malformed Requests", async () => {
+    //Request with wrong password
+    const response = await request(BASE_URL)*/
+/**
+     * @todo
+     * At this time this is out of my knowledge
+     */
+/*.get(`business/tables`)
+      .send({ emailOrPhone: "8777083276", password: "Pritam@rxey123" })
+      .set("Content-Type", "application/json")
+      .set("Accept-Encoding", "gzip, deflate, br")
+      .set("Connection", "keep-alive")
+      .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+    // Request with invalid emailOrPhone
+    const response1 = await request(BASE_URL)
+      .post("/login")
+      .send({ emailOrPhone: "877703276", password: "Pritam@rxefy123" })
+      .set("Content-Type", "application/json")
+      .set("Accept-Encoding", "gzip, deflate, br")
+      .set("Connection", "keep-alive")
+      .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+    expect(response.body.message).toMatch("Invalid password");
+    expect(response1.body.message).toMatch("emailOrPhone must match the following");
+  });
+});
+*/
+describe("Test business/tables/:database", () => {
+  test("TC06 Verify API Response Payload Size", async () => {
+    const database = "application";
+    const falsetoken = "JyotirmoyGhosh1610";
+    const response = await request(BASE_URL)
+      .get(`business/tables/${database}`)
+      .send({ emailOrPhone: "8777083276", password: "Pritam@rxefy123" })
+      .set("Authorization", `Bearer ${falsetoken}`)
+      .set("Content-Type", "application/json")
+      .set("Accept-Encoding", "gzip, deflate, br")
+      .set("Connection", "keep-alive")
+      .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+    expect(response.statusCode).toBe(401);
+  });
+});
 
