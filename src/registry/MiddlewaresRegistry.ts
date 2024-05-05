9600e7f631b23cb80720596c011a8cd97f598437
Nilotpal Saha <nilotpal.saha00@gmail.com>
2024-05-05 10:42:26 +0530

    feat(core): :zap: new changes based on WrappidApp implementation
    new changes based on WrappidApp implementation in server.ts
    
    Ref #117

--- src/registry/MiddlewaresRegistry.ts
+++ src/registry/MiddlewaresRegistry.ts
@@ -1,8 +1,7 @@
-import { CoreMiddlewaresRegistry } from "@wrappid/service-core";
 import * as modulesMiddlewaresRegistry from "../modules/modules.middlewares.registry";
 
 const MiddlewaresRegistry = {
   ...modulesMiddlewaresRegistry.default,
-  ...CoreMiddlewaresRegistry,
 };
+
 export default MiddlewaresRegistry;
